ea6323d9bb8788d19d70a346977e96a402327e32
cris <christty_92@hotmail.com>
2025-05-07 11:46:48 +0200

    result

--- main.js
+++ main.js
@@ -8,13 +8,45 @@ const btnjugar = document.querySelector(".jugar");
 btnjugar.addEventListener("click", (ev) => {
   ev.preventDefault();
   const jugada = jugador.value;
-
+  const jugadaordenador = generarMovimiento();
+  ordenador.innerHTML = jugadaordenador;
   if (jugada === "1") {
-    mostrarjugador.innerHTML = "1";
+    mostrarjugador.innerHTML = "🖐️";
   }
   if (jugada === "2") {
-    mostrarjugador.innerHTML = "2";
+    mostrarjugador.innerHTML = "✊";
   } else if (jugada === "3") {
-    mostrarjugador.innerHTML = "3";
+    mostrarjugador.innerHTML = "✌️";
+  }
+  if (jugadaordenador === "✊" && jugada === "1") {
+    resultado.innerHTML = "PERDISTE";
+  } else if (jugadaordenador === "✌️" && jugada === "2") {
+    resultado.innerHTML = "PERDISTE";
+  } else if (jugadaordenador === "🖐️" && jugada === "3") {
+    resultado.innerHTML = "PERDISTE";
+  } else if (jugadaordenador === jugada) {
+    resultado.innerHTML = "EMPATE";
+  } else {
+    resultado.innerHTML = "GANASTE";
   }
 });
+
+function getRandomNumber(max) { 
+  return Math.ceil(Math.random() * max); 
+}
+
+function generarMovimiento() {
+  const numero = getRandomNumber(10); 
+  if (numero <= 3) {
+    return "✊";
+  } else if (numero >= 7) {
+    return "🖐️";
+  } else {
+    return "✌️";
+  }
+}
+
+console.log(generarMovimiento());
+
+
+
